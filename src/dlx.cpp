7f5b2dbaf32352aec10c916cc455d10f36d70c74
ryan-i-hadiwijaya <ryan.i.hadiwijaya@gdplabs.id>
2020-01-07 15:58:19 +0700

    add sudoku solver

--- src/dlx.cpp
+++ src/dlx.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 typedef pair<int, int> ii;
 
-class Node
-{
+class Node {
 public:
     Node *left;
     Node *right;
@@ -15,6 +14,13 @@ public:
     int rowID;
     int colID;
     int nodeCount;
+};
+
+class RowData {
+public:
+    int row, col, number, id;
+
+    RowData() {}
 };
 
 // Exact Cover problem
@@ -32,6 +38,8 @@ private:
     vector<int> colList[MAX_COL];
     int curRow;
     int base;
+    vector<RowData> rowData;
+    vector<vector<RowData> > answers;
 
     void initHeader() {
         header = new Node();
@@ -105,13 +113,13 @@ private:
         return min_col;
     }
 
-    void printSolutions() {
-        cout << "Printing Solutions: ";
+    void storeSolution() {
+        vector<RowData> ans;
         vector<Node*>::iterator i;
         for (i = solutions.begin(); i != solutions.end(); i++) {
-            cout << (*i)->rowID << " ";
+            ans.push_back(rowData[(*i)->rowID]);
         }
-        cout << "\n";
+        answers.push_back(ans);
     }
 
 public:
@@ -122,9 +130,14 @@ public:
         initHeader();
         curRow = 1;
         base = bs;
+        rowData.clear();
+        rowData.push_back(RowData());
+        answers.clear();
     }
 
-    void addRow(vector<int> cols) {
+    void addRow(vector<int> cols, RowData data = RowData()) {
+        data.id = curRow;
+        rowData.push_back(data);
         sort(cols.begin(), cols.end());
         for (int j = 0; j < cols.size(); j++) {
             int col = cols[j] - base;
@@ -152,7 +165,7 @@ public:
         Node *leftNode;
         Node *column;
         if (header->right == header) {
-            printSolutions();
+            storeSolution();
             return;
         }
         column = getMinColumn();
@@ -171,9 +184,103 @@ public:
         }
         uncover(column);
     }
+
+    vector<vector<RowData> > getSolutions() {
+        return answers;
+    }
+};
+
+class Sudoku {
+private:
+    const static int MAXN = 1050;
+    int n;
+    int arr[MAXN][MAXN];
+    DLX *dlx;
+
+    int getPositionConstraint(int row, int col) {
+        return row * n + col;
+    }
+
+    int getHorizontalConstraint(int row, int number) {
+        return (n * n) + (row * n + number - 1);
+    }
+
+    int getVerticalConstraint(int col, int number) {
+        return (n * n * 2) + (col * n + number - 1);
+    }
+
+    int getBoxConstraint(int row, int col, int number) {
+        int sn = sqrt(n);
+        int rr = row / sn;
+        int cc = col / sn;
+        int box = rr * sn + cc;
+        return (n * n * 3) + (box * n + number - 1);
+    }
+
+    void addDlxRow(int i, int j, int number) {
+        RowData data;
+        data.row = i + 1;
+        data.col = j + 1;
+        data.number = number;
+        vector<int> cols;
+        cols.push_back(getPositionConstraint(i, j));
+        cols.push_back(getHorizontalConstraint(i, number));
+        cols.push_back(getVerticalConstraint(j, number));
+        cols.push_back(getBoxConstraint(i, j, number));
+        dlx->addRow(cols, data);
+    }
+
+public:
+    Sudoku(int N) {
+        n = N;
+        memset(arr, 0, sizeof arr);
+        dlx = new DLX(n * n * 4);
+    }
+
+    void addNumber(int row, int col, int number) {
+        arr[row - 1][col - 1] = number;
+    }
+
+    void printSudoku(string title) {
+        cout << title << "\n";
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                cout << arr[i][j] << "  ";
+            }
+            cout << "\n";
+        }
+        cout << "\n";
+    }
+
+    void printAllSolutions() {
+        vector<vector<RowData> > sol = dlx->getSolutions();
+        for (int i=0; i<sol.size(); i++) {
+            Sudoku s(n);
+            for (int j=0; j<sol[i].size(); j++) {
+                s.addNumber(sol[i][j].row, sol[i][j].col, sol[i][j].number);
+            }
+            s.printSudoku("Solution #" + to_string(i + 1) + ":");
+        }
+    }
+
+    void solve() {
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (arr[i][j] == 0) {
+                    for (int k=1; k<=n; k++) {
+                        addDlxRow(i, j, k);
+                    }
+                } else {
+                    addDlxRow(i, j, arr[i][j]);
+                }
+            }
+        }
+        dlx->search(0);
+    }
 };
 
 DLX *dlx;
+Sudoku *sudoku;
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -229,5 +336,47 @@ int main() {
     dlx->addRow(cols);
 
     dlx->search(0);
+    vector<vector<RowData> > sol = dlx->getSolutions();
+    for (int i=0; i<sol.size(); i++) {
+        cout << "Solution #" << (i + 1) << ":";
+        for (int j=0; j<sol[i].size(); j++) {
+            cout << " " << sol[i][j].id;
+        }
+        cout << "\n";
+    }
+
+    /*
+    Example 2: Sudoku 4x4
+    ? 2 ? ?
+    ? ? ? 1
+    4 ? 3 2
+    ? ? ? ?
+    */
+
+    sudoku = new Sudoku(4);
+    sudoku->addNumber(1, 2, 2);
+    sudoku->addNumber(2, 4, 1);
+    sudoku->addNumber(3, 1, 4);
+    sudoku->addNumber(3, 3, 3);
+    sudoku->addNumber(3, 4, 2);
+    sudoku->solve();
+    sudoku->printSudoku("Original:");
+    sudoku->printAllSolutions();
+
+    /*
+    Example 3: Sudoku 4x4
+    ? ? 1 ?
+    ? 4 ? ?
+    ? ? 2 ?
+    ? 3 ? ?
+    */
+    sudoku = new Sudoku(4);
+    sudoku->addNumber(1, 3, 1);
+    sudoku->addNumber(2, 2, 4);
+    sudoku->addNumber(3, 3, 2);
+    sudoku->addNumber(4, 2, 3);
+    sudoku->solve();
+    sudoku->printSudoku("Original:");
+    sudoku->printAllSolutions();
     return 0;
 }
